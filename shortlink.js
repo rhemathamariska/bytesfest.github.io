cae83753b3ca9ca7ec73fa833f5e840b6d1dfcd2
Palguno Wicaksono <me@icaksh.my.id>
2021-08-19 22:23:46 +0700

    Update shortlink.js

--- shortlink.js
+++ shortlink.js
@@ -1,8 +1,8 @@
 const shorlink = {
-    '/contact' : "https://icaksh.my.id",
+    'contact' : "https://icaksh.my.id",
 };
 
 var getUrl  = window.location.pathname.split('/')[1];
-if (getUrl !== 'assets' || getUrl !== 'files' || getUrl !== ''){
+if (getUrl !== 'assets' && getUrl !== 'files' && getUrl !== ''){
     window.location.replace(shorlink[getUrl]);
 }
